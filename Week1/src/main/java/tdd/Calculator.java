7ed1d369ad8ec503fd11e2aba9db4c729c86857e
sofusk7cdk <sofus@k7c.dk>
2025-01-30 16:09:40 +0100

    Calculator v1

---
+++ Week1/src/main/java/tdd/Calculator.java
@@ -0,0 +1,20 @@
+package tdd;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int substract(int a, int b) {
+        return a-b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public int divide(int a, int b) {
+        return a/b;
+    }
+}
---
+++ Week1/src/test/java/tdd/CalculatorTest.java
@@ -0,0 +1,58 @@
+package tdd;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    //Arrange
+    Calculator calculator = new Calculator();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @DisplayName("Add 2 integers")
+    void add(){
+        int actual = calculator.add(5,5);
+        int expected = 10;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    @DisplayName("Substract 2 integers")
+    void substract(){
+        int actual = calculator.substract(10,5);
+        int expected = 5;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    @DisplayName("Multiply 2 integers")
+    void multiply(){
+        int actual = calculator.multiply(10,5);
+        int expected = 50;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    @DisplayName("Divide 2 integers")
+    void divide() {
+        int actual = calculator.divide(10,5);
+        int expected = 2;
+
+        assertEquals(actual, expected);
+    }
+}
